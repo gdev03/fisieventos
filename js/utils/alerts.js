260318ae1e9d6f1ede3e71ba01628620a8a65b7a
Caballero-Esteban <caballeromaraviesteban@gmail.com>
2026-05-31 15:49:01 -0500

    Update alerts.js

--- js/utils/alerts.js
+++ js/utils/alerts.js
@@ -31,3 +31,72 @@ export function mostrarAlerta(
         });
 
 }
+export function confirmarAccion(
+    titulo,
+    mensaje
+){
+
+    return new Promise((resolve)=>{
+
+        const modal = document.createElement("div");
+
+        modal.classList.add("modal-overlay");
+
+        modal.innerHTML = `
+        <div class="modal-window">
+
+            <h2>${titulo}</h2>
+
+            <p>${mensaje}</p>
+
+            <label>
+                <input
+                    type="checkbox"
+                    id="noMostrar"
+                >
+                No volver a mostrar
+            </label>
+
+            <button id="btnSi">
+                Confirmar
+            </button>
+
+            <button id="btnNo">
+                Cancelar
+            </button>
+
+        </div>
+        `;
+
+        document.body.appendChild(modal);
+
+        document
+            .getElementById("btnSi")
+            .onclick = ()=>{
+
+                const check =
+                    document.getElementById("noMostrar");
+
+                if(check.checked){
+
+                    localStorage.setItem(
+                        "omitirConfirmacion",
+                        true
+                    );
+                }
+
+                modal.remove();
+
+                resolve(true);
+            };
+
+        document
+            .getElementById("btnNo")
+            .onclick = ()=>{
+
+                modal.remove();
+
+                resolve(false);
+            };
+    });
+}
